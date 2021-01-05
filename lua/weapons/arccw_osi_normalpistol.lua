528be18a036977236b385d39e145998a4dbbc6ca
Fesiug <40924091+Fesiug@users.noreply.github.com>
2021-01-05 17:32:59 -0500

    Pistol last fire 'click'

--- lua/weapons/arccw_osi_normalpistol.lua
+++ lua/weapons/arccw_osi_normalpistol.lua
@@ -118,6 +118,9 @@ SWEP.Animations = {
     ["fire_empty"] = {
         Source = "reg_fire_last",
         Time = nil,
+        SoundTable = {
+						{s = "weapons/arccw_osi/pistolslidelock.wav", 	                    t = 0},
+					},
     },
 	["fire_iron"] = {
         Source = "reg_fire_ads",
@@ -126,6 +129,9 @@ SWEP.Animations = {
     ["fire_iron_empty"] = {
         Source = "reg_fire_ads_last",
         Time = nil,
+        SoundTable = {
+						{s = "weapons/arccw_osi/pistolslidelock.wav", 	                    t = 0},
+					},
     },
     ["draw"] = {
         Source = "reg_draw",
