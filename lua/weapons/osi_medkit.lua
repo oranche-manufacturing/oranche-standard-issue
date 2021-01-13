1271d7de85bf9690a4b3e0537c6e8152dfba11b0
Fesiug <40924091+Fesiug@users.noreply.github.com>
2021-01-13 16:56:42 -0500

    shit and fuck

---
+++ lua/weapons/osi_medkit.lua
@@ -0,0 +1,163 @@
+
+AddCSLuaFile()
+
+SWEP.PrintName = "OSI temp Medkit"
+SWEP.Category = "ArcCW - OSI"
+SWEP.Author = "robotboy655 & MaxOfS2D"
+SWEP.Purpose = "Heal people with your primary attack, or yourself with the secondary."
+
+SWEP.Slot = 5
+SWEP.SlotPos = 4
+
+SWEP.Spawnable = true
+
+SWEP.ViewModel = Model( "models/weapons/c_medkit.mdl" )
+SWEP.WorldModel = Model( "models/weapons/w_medkit.mdl" )
+SWEP.ViewModelFOV = 54
+SWEP.UseHands = true
+
+SWEP.Primary.ClipSize = 20
+SWEP.Primary.DefaultClip = 20
+SWEP.Primary.Automatic = true
+SWEP.Primary.Ammo = "none"
+
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = true
+SWEP.Secondary.Ammo = "none"
+
+SWEP.HealAmount = 1 -- Maximum heal amount per use
+SWEP.MaxAmmo = 20 -- Maxumum ammo
+
+local HealSound = Sound( "HealthKit.Touch" )
+local DenySound = Sound( "WallHealth.Deny" )
+
+function SWEP:Initialize()
+
+	self:SetHoldType( "slam" )
+
+	if ( CLIENT ) then return end
+
+	timer.Create( "medkit_ammo" .. self:EntIndex(), 0.2, 0, function()
+		if ( self:Clip1() < self.MaxAmmo ) then self:SetClip1( math.min( self:Clip1() + 1, self.MaxAmmo ) ) end
+	end )
+
+end
+
+function SWEP:PrimaryAttack()
+
+	if ( CLIENT ) then return end
+
+	if ( self.Owner:IsPlayer() ) then
+		self.Owner:LagCompensation( true )
+	end
+
+	local tr = util.TraceLine( {
+		start = self.Owner:GetShootPos(),
+		endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 64,
+		filter = self.Owner
+	} )
+
+	if ( self.Owner:IsPlayer() ) then
+		self.Owner:LagCompensation( false )
+	end
+
+	local ent = tr.Entity
+
+	local need = self.HealAmount
+	if ( IsValid( ent ) ) then need = math.min( ent:GetMaxHealth() - ent:Health(), self.HealAmount ) end
+
+	if ( IsValid( ent ) && self:Clip1() >= need && ( ent:IsPlayer() or ent:IsNPC() ) && ent:Health() < ent:GetMaxHealth() ) then
+
+		self:TakePrimaryAmmo( need )
+
+		ent:SetHealth( math.min( ent:GetMaxHealth(), ent:Health() + need ) )
+		ent:EmitSound( "weapons/arccw_osi/tick"..math.ceil((ent():Health()/ent():GetMaxHealth())*3)..".wav", 60, (ent():Health()/ent():GetMaxHealth())*100, 0.5 )
+
+		self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
+
+		self:SetNextPrimaryFire( CurTime() + 0.05 )
+		self.Owner:SetAnimation( PLAYER_ATTACK1 )
+
+		-- Even though the viewmodel has looping IDLE anim at all times, we need this to make fire animation work in multiplayer
+		timer.Create( "weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function() if ( IsValid( self ) ) then self:SendWeaponAnim( ACT_VM_IDLE ) end end )
+
+	else
+
+		self.Owner:EmitSound( DenySound )
+		self:SetNextPrimaryFire( CurTime() + 1 )
+
+	end
+
+end
+
+function SWEP:SecondaryAttack()
+
+	if ( CLIENT ) then return end
+
+	local ent = self.Owner
+
+	local need = self.HealAmount
+	if ( IsValid( ent ) ) then need = math.min( ent:GetMaxHealth() - ent:Health(), self.HealAmount ) end
+
+	if ( IsValid( ent ) && self:Clip1() >= need && ent:Health() < ent:GetMaxHealth() ) then
+
+		self:TakePrimaryAmmo( need )
+
+		ent:SetHealth( math.min( ent:GetMaxHealth(), ent:Health() + need ) )
+		ent:EmitSound( "weapons/arccw_osi/tick"..math.ceil((self:GetOwner():Health()/self:GetOwner():GetMaxHealth())*3)..".wav", 60, (self:GetOwner():Health()/self:GetOwner():GetMaxHealth())*100, 0.5 )
+		
+		
+
+		self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
+
+		self:SetNextSecondaryFire( CurTime() + 0.05 + ( 0.1 * ( self:GetOwner():Health() / self:GetOwner():GetMaxHealth() ) ) )
+		self.Owner:SetAnimation( PLAYER_ATTACK1 )
+
+		timer.Create( "weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function() if ( IsValid( self ) ) then self:SendWeaponAnim( ACT_VM_IDLE ) end end )
+
+	else
+
+		ent:EmitSound( "weapons/arccw_osi/tick4.wav" )
+		self:SetNextSecondaryFire( CurTime() + .5 )
+
+	end
+
+end
+
+function SWEP:DrawHUD()
+	surface.SetFont("ArcCW_26")
+	surface.SetTextPos(ScrW() / 2, 50 + 26 * 0)
+	surface.SetTextColor(255, 255, 255, 255)
+	surface.DrawText(self:GetOwner():Health())
+	
+	surface.SetFont("ArcCW_26")
+	surface.SetTextPos(ScrW() / 2, 50 + 26 * 4)
+	surface.SetTextColor(255, 255, 255, 255)
+	surface.DrawText(math.ceil((self:GetOwner():Health()/self:GetOwner():GetMaxHealth())*3))
+end
+
+function SWEP:OnRemove()
+
+	timer.Stop( "medkit_ammo" .. self:EntIndex() )
+	timer.Stop( "weapon_idle" .. self:EntIndex() )
+
+end
+
+function SWEP:Holster()
+
+	timer.Stop( "weapon_idle" .. self:EntIndex() )
+
+	return true
+
+end
+
+function SWEP:CustomAmmoDisplay()
+
+	self.AmmoDisplay = self.AmmoDisplay or {}
+	self.AmmoDisplay.Draw = true
+	self.AmmoDisplay.PrimaryClip = self:Clip1()
+
+	return self.AmmoDisplay
+
+end
