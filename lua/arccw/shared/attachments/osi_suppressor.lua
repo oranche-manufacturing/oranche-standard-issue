b03d038974f18efcb531e270a41cb3afac2c8e56
Cylowalkee <76599609+Cylowalkee@users.noreply.github.com>
2021-01-21 18:46:31 +0300

    the P

--- lua/arccw/shared/attachments/osi_suppressor.lua
+++ lua/arccw/shared/attachments/osi_suppressor.lua
@@ -1,5 +1,5 @@
 att.PrintName = "Sovetsky Suppressor"
-att.Icon = Material("entities/osi_supressor.png")
+att.Icon = Material("entities/osi_suppressor.png")
 att.Description = "Patented Russian muffler design, made for when you don't want to wake up all of Moscow while doing KGB work."
 att.Desc_Pros = {
 }
